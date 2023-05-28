5fb716ff2457ace4c3f23295c07beaae7b7e3087
rmnhernan <60717000+rmnhernan@users.noreply.github.com>
2023-05-28 05:22:48 -0300

    ADD -> files_handling_functions.py - moved file funcs. there

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import exam_functions as funcs
 from menu_functions import main_app 
+from files_handling_functions import read_json_file
 
 #### PROGAM START ####
 
-players_list = funcs.read_json_file("dt.json", "jugadores")
+#players_list = funcs.read_json_file("dt.json", "jugadores")
+players_list = read_json_file("dt.json", "jugadores")
+
 
 main_app(players_list)
 
